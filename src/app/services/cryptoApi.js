5a24460b8f025755a279d8080cabdfbad1ce6170
Nikita <nickpelmog2003@gmail.com>
2023-06-21 01:27:48 +0300

    feat: edit redux settings

--- src/app/services/cryptoApi.js
+++ src/app/services/cryptoApi.js
@@ -12,19 +12,23 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const cryptoApi = createApi({
   reducerPath: "cryptoApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://moviesdatabase.p.rapidapi.com",
+    baseUrl: "https://coinranking1.p.rapidapi.com",
     prepareHeaders: (header) => {
-      header.set("X-RapidAPI-Host", "moviesdatabase.p.rapidapi.com");
+      header.set("X-RapidAPI-Host", "coinranking1.p.rapidapi.com");
       header.set(
         "X-RapidAPI-Key",
-        "eb71184572msh8f332283060f7cbp1f341fjsnc4685458b6c2"
+        "7d453acff4msh6276223c5675ac2p10c93ejsn2ead53f34641"
       );
+      // header.set(
+      //   "x-access-token",
+      //   "coinranking21292cbb5e73cd72f912fab5b762129de649b0013c285868"
+      // );
       return header;
     },
   }),
   endpoints: (builder) => {
     getCryptos: builder.query({
-      query: () => `/actors/random`,
+      query: () => "/reference-currencies?limit=10",
     });
   },
 });
